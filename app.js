6360fcdaa6a6738fac094564528e929b45a79f76
Fahmi Akbar <fahmialiasakbar@gmail.com>
2022-12-15 20:37:09 +0700

    Mouse Events

--- app.js
+++ app.js
@@ -6,15 +6,24 @@ const app = Vue.createApp({
             author: 'Leonardo de Miawo',
             year: 2020,
             show_miaw: true,
+            x: 0,
+            y: 0,
         }
     },
     methods: {
-
         change_title(title) {
             this.title = title
         },
         show_hide() {
             this.show_miaw = !this.show_miaw
+        },
+        handle_event(e, data) {
+            console.log(e, e.type)
+            console.log("data: " + data)
+        },
+        handle_mouse_move(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
