2799f01932c691474eaa65b23b7e2d761280bc71
Fahmi Akbar <fahmialiasakbar@gmail.com>
2022-12-16 10:47:00 +0700

    Dynamic Class

--- books.js
+++ books.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
             store: 'Demak Book Collection',
             url: 'https://www.tokopedia.com/demakstore',
             books: [
-                { title: 'Menjadi Superman', author: 'MurzCat', image: 'img/panda.jpg' },
-                { title: 'Kucing yang baik', author: 'Tanggon', image: 'img/miaw.jpg' },
-                { title: 'Langkah mencuri ikan', author: 'Fahmi', image: 'img/anjing.jpg' },
+                { title: 'Menjadi Superman', author: 'MurzCat', image: 'img/panda.jpg', is_best_seller: false },
+                { title: 'Kucing yang baik', author: 'Tanggon', image: 'img/miaw.jpg', is_best_seller: true },
+                { title: 'Langkah mencuri ikan', author: 'Fahmi', image: 'img/anjing.jpg', is_best_seller: false },
             ]
         }
     },
